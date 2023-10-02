73ce18cab062439afe97b6d6e9ad8ee59bd2b211
JongHoon <qnm541@gmail.com>
2023-10-02 23:34:12 +0900

    [feature] Constants enum 추가

---
+++ TodayFastComplete/Resources/Constants/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  TodayFastComplete
+//
+//  Created by JongHoon on 9/30/23.
+//
+
+import UIKit
+
+enum Constants { 
+    
+    // MARK: - Color
+    enum Color {
+        static let background = UIColor(resource: .background)
+    }
+    
+    // MARK: - Icon
+    enum Icon {
+        static let timer = UIImage(systemName: "timer")
+    }
+    
+}
+
