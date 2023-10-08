7d55bffd0016f5bf8a7f9b7eca7beea256bbfdb7
JongHoon <qnm541@gmail.com>
2023-10-08 20:28:28 +0900

    [feature] recommend fast routine entity 추가

---
+++ TodayFastComplete/Domain/Entities/RecommendFastRoutine.swift
@@ -0,0 +1,23 @@
+//
+//  RecommendFastRoutine.swift
+//  TodayFastComplete
+//
+//  Created by JongHoon on 10/8/23.
+//
+
+enum RecommendFastRoutine: CaseIterable {
+    case fastTime16
+    case fastTime12
+    case fastTime23
+    
+    var fastRoutine: FastRoutine {
+        switch self {
+        case .fastTime16:
+            FastRoutine(fastingTime: 16, mealCount: 2, image: Constants.Imgage.fasting)
+        case .fastTime12:
+            FastRoutine(fastingTime: 12, mealCount: 3, image: Constants.Imgage.fasting)
+        case .fastTime23:
+            FastRoutine(fastingTime: 23, mealCount: 1, image: Constants.Imgage.fasting)
+        }
+    }
+}
