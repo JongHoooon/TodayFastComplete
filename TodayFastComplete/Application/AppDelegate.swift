eb6f6aba4fb8d3c8a866eeb99439d7a4f42fb982
JongHoon <qnm541@gmail.com>
2023-10-13 18:52:12 +0900

    [feature] 알림 권한 요청 처리

--- TodayFastComplete/Application/AppDelegate.swift
+++ TodayFastComplete/Application/AppDelegate.swift
@@ -14,7 +14,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 알림 권한 설정
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current()
+            .requestAuthorization(
+                options: [.alert, .sound, .badge],
+                completionHandler: { success, error in
+                    if let error {
+                        Log.error(error)
+                    } else if success == true {
+                        Log.info("request authorization authorized")
+                    } else {
+                        Log.info("request authorization rejected")
+                    }
+                }
+            )
+        
         return true
     }
 
@@ -25,8 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        
         return UISceneConfiguration(
             name: "Default Configuration",
             sessionRole: connectingSceneSession.role
@@ -37,8 +52,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+
     }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
 }
