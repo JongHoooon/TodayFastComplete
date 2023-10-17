d3d6e668b5093bba2184d50ba80b1069d1582f24
JongHoon <qnm541@gmail.com>
2023-10-17 15:57:13 +0900

    [feature] recordDIContainer 추가

---
+++ TodayFastComplete/Application/DIContainer/RecordDIContainer.swift
@@ -0,0 +1,12 @@
+//
+//  RecordDIContainer.swift
+//  TodayFastComplete
+//
+//  Created by JongHoon on 10/17/23.
+//
+
+import UIKit
+
+final class RecordDIContainer: RecordCoordinatorDependencies {
+    
+}
