fcf6d5b18db2f89b37ba796ddb5418bb861c01d3
JongHoon <qnm541@gmail.com>
2023-11-10 15:47:01 +0900

    [refactor]  record validate error 분리

---
+++ TodayFastComplete/Presentation/Flows/RecordFlow/WriteFastView/ViewModel/RecordValidateError.swift
@@ -0,0 +1,11 @@
+//
+//  RecordValidateError.swift
+//  TodayFastComplete
+//
+//  Created by JongHoon on 11/10/23.
+//
+
+enum RecordValidateError: Error {
+    case badFastTime
+    case badWeight
+}
